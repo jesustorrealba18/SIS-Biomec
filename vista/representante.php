8df024bab32020829660d1d268096fd6a29a5c7f
jesustorrealba18 <jesustorrealbaa14@gmail.com>
2026-06-24 21:47:55 -0400

    Logo en la parte superior de la ventana y mejoras en las validaciones de Atletas

--- vista/representante.php
+++ vista/representante.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
+		<link rel="icon" type="image/png" href="assets/img/logo_nadador.png">
     <title>Representantes | SGRD</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
